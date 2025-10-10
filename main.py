0fb3c127943625499e1b41d7c9fbd37e917d8640
IrakliGLD <galdavika@gmail.com>
2025-10-10 21:03:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def healthz():
 if __name__ == "__main__":
     import uvicorn, os
     port = int(os.getenv("PORT", "8000"))
-    print(f"Starting app on port {port}")
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
