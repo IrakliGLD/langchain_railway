93130c9cb8da78b0bd46e7a22ffe0ce85457ed6f
IrakliGLD <galdavika@gmail.com>
2025-10-10 23:50:51 +0400

    Update main.py

--- main.py
+++ main.py
@@ -399,5 +399,4 @@ def ask_post(
 # -----------------------------
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", "3000"))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=3000)
